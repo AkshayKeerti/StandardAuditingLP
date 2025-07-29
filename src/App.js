4faf873dbc1153c47bc2cf3f0f9f6e171cca1933
Akshaykeerti <akshaykeerti@gmail.com>
2025-07-29 19:28:42 +0400

    Update App.js with latest changes

--- src/App.js
+++ src/App.js
@@ -52,7 +52,6 @@ function App() {
       <ListedAuditors />
       <GovernmentAgencies />
       <ClientLogos />
-      <CertificationsTrust />
       <Testimonials />
       <FAQ />
       <LeadFormSection />
