95e092a5a25fa7297ae85c364520f0e95aee6ff7
Akshaykeerti <akshaykeerti@gmail.com>
2025-07-30 12:01:44 +0400

    Update Header component changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -68,12 +68,7 @@ const Header = () => {
               </div>
             </div>
 
-            {/* Mobile Menu Button */}
-            <button className="lg:hidden text-gray-700 self-end p-2 hover:bg-gray-100 rounded-lg transition-colors duration-200">
-              <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
-              </svg>
-            </button>
+
           </div>
         </div>
       </div>
